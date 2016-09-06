8fd47369fc986aa59f414f3f9797ac4e65c1a8b0
Craig <cgleckman@gmail.com>
2016-09-05 19:01:24 -0700

    added basic functionality to display a list of books on the main screen

--- BookStore/BooksTableViewController.cs
+++ BookStore/BooksTableViewController.cs
@@ -1,13 +1,66 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
 using UIKit;
 
 namespace BookStore
 {
     public partial class BooksTableViewController : UITableViewController
     {
+        List<Book> bookList;
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
+            bookList = new List<Book>();
+
+            bookList.Add(new Book()
+            {
+                name = "Harry Potter and the Sorcerer's Stone",
+                author = "J.K Rowling",
+                publisher = "Bloomsbury",
+                year = 1997
+            });
+
+            bookList.Add(new Book()
+            {
+                name = "Harry Potter and the Chamber of Secrets",
+                author = "J.K Rowling",
+                publisher = "Bloomsbury",
+                year = 1998
+            });
+
+            bookList.Add(new Book()
+            {
+                name = "Harry Potter and the Prisoner of Azkaban",
+                author = "J.K Rowling",
+                publisher = "Bloomsbury",
+                year = 1999
+            });
         }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return bookList.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("Book");
+            var data = bookList[indexPath.Row];
+            cell.TextLabel.Text = data.name;
+            return cell;
+        }
+    }
+
+    public class Book
+    {
+        public string name;
+        public string author;
+        public string publisher;
+        public int year;
     }
 }
