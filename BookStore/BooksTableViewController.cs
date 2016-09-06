e1c1b10ca0f1b89d227bd0b34879b1e0d716aab6
Craig <cgleckman@gmail.com>
2016-09-05 18:27:29 -0700

    init commit
    initial bare bones application for bookstore

---
+++ BookStore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore
+{
+    public partial class BooksTableViewController : UITableViewController
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
