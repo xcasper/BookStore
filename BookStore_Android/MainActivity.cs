ec7c855ecdc01c9b9aa05e7632b712e9b4888a81
Craig <cgleckman@gmail.com>
2016-09-07 19:58:29 -0700

    barebones android app with ListActivity

---
+++ BookStore_Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace BookStore_Android
+{
+    [Activity(Label = "BookStore_Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
+
